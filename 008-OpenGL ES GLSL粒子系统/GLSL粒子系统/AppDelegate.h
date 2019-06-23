84349759b1ba9543fcee8c099fa5bf0d1db9cb2d
denglei <denglei@sdbattery.com>
2019-06-23 13:14:39 +0800

    008

---
+++ 008-OpenGL ES GLSL粒子系统/GLSL粒子系统/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLSL粒子系统
+//
+//  Created by denglei on 2019/6/20.
+//  Copyright © 2019 soudian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 008-OpenGL ES GLSL粒子系统/GLSL粒子系统/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  GLSL粒子系统
+//
+//  Created by denglei on 2019/6/20.
+//  Copyright © 2019 soudian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+
+@end
+
